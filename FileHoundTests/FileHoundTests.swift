bf0f5d829cb36dcb2e53526a6886d189989778d3
VanJay <vanjay.dev@gmail.com>
2026-04-08 07:59:49 +0800

    完善工程文件与清理用户态配置

---
+++ FileHoundTests/FileHoundTests.swift
@@ -0,0 +1,19 @@
+//
+//  FileHoundTests.swift
+//  FileHoundTests
+//
+//  Created by VanJay on 2026/4/8.
+//
+
+import Testing
+@testable import FileHound
+
+struct FileHoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
